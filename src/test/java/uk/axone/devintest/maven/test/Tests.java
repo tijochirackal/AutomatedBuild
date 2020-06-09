04224de443763ba4be25262bfe6cadf38c3b6195
tijochirackal <tijochirackaltom@gmail.com>
2020-06-09 09:36:24 +0100

    setup all test to pass

--- src/test/java/uk/axone/devintest/maven/test/Tests.java
+++ src/test/java/uk/axone/devintest/maven/test/Tests.java
@@ -18,12 +18,12 @@ public class Tests {
     @Test
     public void test3(){
         System.out.println("running test 3");
-        Assert.fail();
+
     }
     @Test
     public void test4(){
         System.out.println("running test 4");
-        Assert.fail();
+
     }
 
 }
